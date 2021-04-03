c12249c9573be3ad6f064316414062e09d56d43d
ridhdhish <ridhdhishdesai292000@gmail.com>
2021-04-03 22:55:02 +0530

    initial

---
+++ WebAPI/WebAPI/Controllers/CarController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ConsoleApp1;
+
+namespace WebAPI.Controllers
+{
+    public class CarController : ApiController
+    {
+        public CarController()
+        {
+        }
+
+        public IHttpActionResult Get(string userid, int carid)
+        {
+            Car temp = null;
+            using (CarEntities entities = new CarEntities())
+            {
+                try
+                {
+                    temp = entities.Cars.Where(t => t.Id == carid).Where(t => t.email == userid).FirstOrDefault();
+                    if (temp == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+
+                        return Ok(entities.Cars.Where(t => t.Id == carid).Where(t => t.email == userid).FirstOrDefault());
+                    }
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Can not find ToDo");
+                }
+            }
+        }
+        public IEnumerable<Car> Get(string userid)
+        {
+            using (CarEntities entities = new CarEntities())
+            {
+                return entities.Cars.Where(t => t.email == userid).ToList();
+            }
+        }
+
+        public IEnumerable<Car> Get()
+        {
+            using (CarEntities entities = new CarEntities())
+            {
+                return entities.Cars.ToList();
+            }
+        }
+
+        public IHttpActionResult Post([FromBody] Car car)
+        {
+            using (CarEntities entities = new CarEntities())
+            {
+                try
+                {
+                    entities.Cars.Add(car);
+                    entities.SaveChanges();
+                    return Ok(entities.Cars.Where(t => t.email == car.email).ToList());
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Can not add the car");
+                }
+            }
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put(string userid, [FromBody] Car todo)
+        {
+            Car temp = null;
+            using (CarEntities entities = new CarEntities())
+            {
+
+                try
+                {
+                    temp = entities.Cars.Where(t => t.name == todo.name).FirstOrDefault();
+                    if (temp == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {                        
+                        temp.email = todo.email;
+
+
+                        entities.SaveChanges();
+                        return Ok(entities.Cars.Where(t => t.email == userid).ToList());
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Can not insert Car");
+                }
+            }
+        }
+
+        public IHttpActionResult Put(string userid, int carId)
+        {
+            Car temp = null;
+            using (CarEntities entities = new CarEntities())
+            {
+
+                try
+                {
+                    temp = entities.Cars.Where(t => t.Id == carId).FirstOrDefault();
+                    if (temp == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        temp.email = "Not rented";
+
+
+                        entities.SaveChanges();
+                        return Ok(entities.Cars.Where(t => t.email == userid).ToList());
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Can not insert Car");
+                }
+            }
+        }
+
+        public IHttpActionResult Delete(string userid, int id)
+        {
+            Car temp;
+            using (CarEntities entities = new CarEntities())
+            {
+
+                try
+                {
+                    temp = entities.Cars.Where(t => t.Id == id).Where(t => t.email == userid).FirstOrDefault();
+                    if (temp == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        entities.Cars.Remove(entities.Cars.Where(t => t.Id == id).Where(t => t.email == userid).FirstOrDefault());
+                        entities.SaveChanges();
+                        return Ok(entities.Cars.Where(t => t.email == userid).ToList());
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Unable to Delete todo");
+                }
+            }
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/UserController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ConsoleApp1;
+
+namespace WebAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IHttpActionResult Get(string id)
+        {
+            User temp = null;
+            using (CarEntities entities = new CarEntities())
+            {
+                try
+                {
+                    temp = entities.Users.FirstOrDefault(u => u.email == id);
+                    if (temp == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+
+                        return Ok(entities.Users.Where(u => u.email == id).FirstOrDefault());
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Can not find User");
+                }
+            }
+        }
+        public IEnumerable<User> Get()
+        {
+            using (CarEntities entities = new CarEntities())
+            {
+                return entities.Users.ToList();
+            }
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put(string id, [FromBody] User user)
+        {
+            User temp = null;
+            using (CarEntities entities = new CarEntities())
+            {
+
+                try
+                {
+                    temp = entities.Users.FirstOrDefault(u => u.email == id);
+                    if (temp == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        temp.password = temp.password;
+                        temp.email = user.email;
+                        
+
+                        entities.SaveChanges();
+                        return Ok(entities.Users.ToList());
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Can not update User");
+                }
+            }
+        }
+
+        public IHttpActionResult Delete(string id)
+        {
+            User temp;
+            using (CarEntities entities = new CarEntities())
+            {
+
+                try
+                {
+                    temp = entities.Users.FirstOrDefault(u => u.email == id);
+                    if (temp == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        entities.Users.Remove(entities.Users.FirstOrDefault(u => u.email == id));
+                        entities.SaveChanges();
+                        return Ok(entities.Users.ToList());
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Unable to Delete user");
+                }
+            }
+        }
+
+        public IHttpActionResult Get(string id, string password)
+        {
+            User temp;
+            using (CarEntities entities = new CarEntities())
+            {
+                try
+                {
+                    temp = entities.Users.FirstOrDefault(u => u.email == id);
+                    if (temp == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        if (temp.email.Equals(id) && temp.password.Equals(password))
+                        {
+                            return Ok("Login Successful");
+                        }
+                        else
+                        {
+                            return BadRequest("Login Failed");
+                        }
+
+                    }
+                }
+
+
+                catch (Exception e)
+                {
+                    return BadRequest("Login Failed");
+                }
+            }
+        }
+
+
+        public IHttpActionResult Post([FromBody] User user)
+        {
+            using (CarEntities entities = new CarEntities())
+            {
+                try
+                {
+                    if (entities.Users.FirstOrDefault(u => u.email == user.email) == null)
+                    {
+                        entities.Users.Add(user);
+                        entities.SaveChanges();
+                        return Ok(entities.Users.ToList());
+                    }
+                    else
+                    {
+                        return BadRequest("Can not insert the User");
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Can not insert the User");
+                }
+            }
+        }
+    }
+
+}
